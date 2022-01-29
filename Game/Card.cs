e64b546dcd2fc323d4afdc686aaeb8a39dd7d67f
Roran47 <ekcook@live.com>
2022-01-28 21:47:23 -0800

    update to merge

--- Game/Card.cs
+++ Game/Card.cs
@@ -1,8 +1,13 @@
+
 using System;
 using System.Collections.Generic;
 
 namespace Game;
 public class Card {
+
+    // create methed called "createCardNumber" that will create a number for the varible called 'curenntCard' this will also move the number that was in
+    // curenntCard to  
+
     Random random = new Random();
     
     // create methed called "createCardNumber" that will create a number for the varible called 'currentCard' this will also move the number that was in 
@@ -13,3 +18,4 @@ public class Card {
         Console.Write($"Card is: {currentCard}");
     }
 }
+
