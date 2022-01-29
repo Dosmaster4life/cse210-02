21a30f1ee8b78f5ec220bf4a491aa2f3cb48ad77
Roran47 <ekcook@live.com>
2022-01-28 20:55:31 -0800

    guide part 0ne

---
+++ Game/Card.cs
@@ -0,0 +1,3 @@
+
+// create methed called "createCardNumber" that will create a number for the varible called 'curenntCard' this will also move the number that was in 
+
---
+++ Game/Player.cs
@@ -0,0 +1,5 @@
+// create methed called "lifePointsCounter" that will output our an intinger type varible called 'lifePoints' 
+
+// create methed called "makeGuess" that will pull the users input and store it in the varible  called 'highLow'
+
+// create methed called "checkAnswer" that will take 'highLow' and 'curenntCard' to see if the user guess is high or low. 
