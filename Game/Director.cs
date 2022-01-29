34343504a6381815c4787bdb477de6b1b6058b52
= <jefferysuperman@gmail.com>
2022-01-28 20:58:36 -0700

    Minor changes

--- Game/Director.cs
+++ Game/Director.cs
@@ -5,31 +5,24 @@ namespace Game
 {
 
     public class Director {
-        bool playingNow = true;
-        bool highOrLow = false;
+        public static bool playingNow = true;
+        string response = "";
+        
                public void StartGame()
         {
             while (playingNow) // Call methods and functions from here, infinite loop until PlayNow is false;
             {
-               getInput();
-               Player.makeGuess(getResonse);
+              
+               
+               Player player = new Player();
+               player.makeGuess(getInput());
 
             }
         }
-        public void getInput() {
+        public string getInput() {
             Console.Write("The card is " + "Function to get card here" + "\n Higher or Lower? [h/l]");
             string getResponse = Console.ReadLine();
-            if(getResponse == "e") {
-                playingNow = false;
-            
-            }else if(getResponse == "h") {
-                highOrLow = true;
-            }else if(getResponse == "l") {
-                highOrLow = false;
-            }
-
-          
-           
+            return getResponse;
         }
     }
 }
--- Game/Player.cs
+++ Game/Player.cs
@@ -4,4 +4,17 @@ using System.Collections.Generic;
 namespace Game;
 public class Player {
 
+public int lifePoints;
+public bool highOrLow = false;
+public void makeGuess(string getResponse) {
+    if(getResponse == "e") {
+                Director.playingNow = false;
+            }else if(getResponse == "h") {
+                highOrLow = true;
+            }else if(getResponse == "l") {
+                highOrLow = false;
+            }
+
+}
+
 }
