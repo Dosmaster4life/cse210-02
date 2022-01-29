9ce9eb7821ed9c07c4b7a3e267825049b69a75e9
= <jefferysuperman@gmail.com>
2022-01-28 20:44:48 -0700

    Small class change

--- Game/Director.cs
+++ Game/Director.cs
@@ -6,24 +6,30 @@ namespace Game
 
     public class Director {
         bool playingNow = true;
-         public void StartGame()
+        bool highOrLow = false;
+               public void StartGame()
         {
             while (playingNow) // Call methods and functions from here, infinite loop until PlayNow is false;
             {
                getInput();
+               Player.makeGuess(getResonse);
 
             }
         }
         public void getInput() {
             Console.Write("The card is " + "Function to get card here" + "\n Higher or Lower? [h/l]");
             string getResponse = Console.ReadLine();
-           if(getResponse == "h") {
+            if(getResponse == "e") {
+                playingNow = false;
+            
+            }else if(getResponse == "h") {
+                highOrLow = true;
+            }else if(getResponse == "l") {
+                highOrLow = false;
+            }
 
-           } else if(getResponse == "l") {
-
-           }else { // if user chooses somthing other than h or l
-
-           }
+          
+           
         }
     }
 }
